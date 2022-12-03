0f38ed521199c90a6ebe458e9002fe8ae3d81001
İlayda Cundar <ilayda.cundar@akinon.com>
2022-12-03 18:36:12 +0300

    Added web-socket configuration

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -4,6 +4,12 @@ const server = require("http").createServer(app);
 
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send('Server is running');
+});
+
 const io = require("socket.io")(server, {
   cors: {
     origin:"*",
@@ -11,11 +17,21 @@ const io = require("socket.io")(server, {
   }
 })
 
-const PORT = process.env.PORT || 5000;
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
 
-app.get("/", (req, res) => {
-  res.send('Server is running');
-});
+  socket.on('disconnect', () => {
+    socket.broadcast.emit("callended"); 
+  });
+
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+  });
+
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  })
+})
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
 
---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Screen from './components/Screen';
+import Settings from './components/Settings';
+
+const App = () => {
+
+  return (
+    <div>
+      <Screen/>
+      <Settings/>
+    </div>
+  )
+}
+
+export default App;
